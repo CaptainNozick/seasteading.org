5688e0e441a10ed12d67de8068dbeb8c6d8bbe97
kamilpelka <kamilpelka@ea5bd63e-ab88-11dd-b08a-b13dd1f1ca2d>
2009-01-23 10:08:55 +0000

    Pulled out sidebar to a separate file, added breadcrumbs.

--- sites/all/themes/seastead/page.tpl.php
+++ sites/all/themes/seastead/page.tpl.php
@@ -98,56 +98,32 @@
 		
 		<div id="content-1"<?php if ( arg(0) == 'blog' || $node->type == 'blog' || arg(1) == 'press-releases' || $node->type == 'press_release' ) : ?> class="post"<?php endif ?>>
 			
-			<div id="sidebar">
-				
-				<? if (
-					arg(0) != 'user' &&
-					!(arg(0) == 'node' && arg(1) == 'add') &&
-					!(arg(0) == 'node' && arg(1) == 'edit') &&
-					arg(0) != 'blog' &&
-					arg(0) != 'month' && 
-					$node->type != 'blog' &&
-					!$at_blog_term &&
-					$menu_root_title['title'] != 'Content' &&
-					$menu_root_title['title'] != 'View' &&
-					$menu_root_title['title'] != 'Registered Users'
-				) : ?>
-				
-					<div class="sub-menu">
-						<div id="block-menu-83" class="clear-block block block-menu">
-							<h2><?=$menu_root_title['title']?></h2>
-							<div class="content">
-								<?=theme('menu_tree',$menu_root)?>
-							</div>
-						</div>
-					</div>
-				
-				<?php endif ?>
-				
-				<?php if ($sidebar_left): ?>
-					<?=$sidebar_left ?>
-				<?php endif; ?>
-				
-				<?php if ( arg(0) == 'blog' || $node->type == 'blog' || arg(0) == 'month' || $at_blog_term || (arg(0) == 'stay-in-touch' && arg(1) == 'press-releases') || $node->type == 'press_release' || $commented_node->type == 'press_release') : ?>
-				
-					<?php
-						$rss_url = '';
-						if (arg(0) == 'blog' || $node->type == 'blog' || arg(0) == 'month' || $at_blog_term) $rss_url = base_path() .  'blog/feed/full';
-						if ((arg(0) == 'stay-in-touch' && arg(1) == 'press-releases') || $node->type == 'press_release' || $commented_node->type == 'press_release') $rss_url = base_path() . 'stay-in-touch/press-releases/feed'
-					?>
-				
-					<div id="subscribe" class="clear-block block">
-						<div class="content">
-							<p><strong>Subscribe by:</strong> <a href="<?=$rss_url?>">RSS</a><!-- &nbsp;|&nbsp;  <a href="#">E-mail</a>--> <a href="<?=$rss_url?>"><img width="16" height="16" title="Syndicate content" alt="Syndicate content" src="<?=base_path()?>misc/feed.png"/></a></p>
-						</div>
-					</div>
-				
-				<?php endif ?>
-				
-			</div>
+			<?php if (!$at_forum) include('sidebar.tpl.php') ?>
 			
 			<div id="content-wrapper">
 				<div id="content-wrapper-1">
+				
+					<?php if ($breadcrumb): ?>
+						<div class="breadcrumb">
+							<strong>You are here:</strong>
+							<?php if ($node->type == 'forum') : ?>
+								<?php 
+									
+									$bc = array();
+									$bc[] = array('path' => $_GET['q']);
+									menu_set_location($bc);
+								?>
+							<?php elseif(arg(0) == 'blog'): ?>
+								<?php $title = 'Captain\'s Blog'; ?>
+							<?php elseif(arg(0) == 'comment' && arg(1) == 'reply'): ?>
+								<?php $title = 'Reply'; ?>
+							<?php endif ?>
+							<?=$breadcrumb;?>
+							<?php if ($title): ?>
+								<span>&gt;</span> <?=$title?>
+							<?php endif ?>
+						</div>
+					<?php endif; ?>
 				
 					<?php if ($tabs): print '<div id="tabs-wrapper">'; endif; ?>
           <?php if ($title): print '<h2'. ($tabs ? ' class="with-tabs"' : '') .'>'. $title .'</h2>'; endif; ?>
@@ -161,6 +137,8 @@
 				
 				</div>
 			</div>
+			
+			<?php if ($at_forum) include('sidebar.tpl.php') ?>
 			
 		</div>
 		
