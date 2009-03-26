d7b3330a7cfdeee06ff75fe1753543b84657c6c9
kamilpelka <kamilpelka@ea5bd63e-ab88-11dd-b08a-b13dd1f1ca2d>
2009-03-26 13:23:18 +0000

    Changes required by new 1st level items in the main menu.

--- sites/all/themes/seastead/scripts/application.js
+++ sites/all/themes/seastead/scripts/application.js
@@ -1,11 +1,17 @@
 /* Emulate li:hover in menu for IE6 (although jQuery's browser detection sucks, so most probably IE7 will catch on too - but it should work ok anyway) */
-
 $(document).ready(function(){
-	if ($.browser.msie && $.browser.version <= 6) {
+	if ($.browser.msie) {
 		$('#block-menu-2 .content > ul.menu li ul.menu').each(function(i,val){ $(val).css('display','none') });
 		$('#block-menu-2 .content > ul.menu li:has(ul.menu)').each(function(i,val){
 			$(val).bind('mouseenter',function(e){
 				$('ul.menu:first',this).css('display','block');
+				$(function() {
+					var zIndexNumber = 1000;
+					$('div').each(function() {
+						$(this).css('zIndex', zIndexNumber);
+						zIndexNumber -= 10;
+					});
+				});
 			});
 			$(val).bind('mouseleave',function(e){
 				$('ul.menu:first',this).css('display','none');
