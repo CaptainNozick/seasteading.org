c1573c21d059cde22c3f11351bd5ef1d2c3ded9f
Nozick <nozick@vsps-macbook-pro-15.local>
2009-04-26 11:43:05 -0400

    Remove dead login form fields

--- sites/all/themes/seastead/user-login-bar.tpl.php
+++ sites/all/themes/seastead/user-login-bar.tpl.php
@@ -9,20 +9,16 @@
 		<?php else : ?>
 			<form action="<?=base_path()?>user/login/?<?=drupal_get_destination()?>" method="post" id="user-login-form" accept-charset="UTF-8">
 				<div>
-					<!--<input type="hidden" name="edit[destination]" value="user" />-->
 					<a href="<?php print base_path() ?>user/register" title="Register" class="register"><span>Register</span></a>
 					<span class="or">or</span>
 					<div class="form-item">
-						<!--<input type="text" maxlength="60" name="edit[name]" id="edit-name" size="15" value="Username:" class="form-text required" />-->
 						<input type="text" maxlength="60" name="name" id="edit-name" size="15" value="" class="form-text required" />
 					</div>
 					<div class="form-item">
-						<!--<input type="password" name="edit[pass]" id="edit-pass" maxlength="60" size="15" class="form-text required" />-->
 						<input type="password" name="pass" id="edit-pass" maxlength="60" size="15" class="form-text required" />
 					</div>
 					<input type="image" src="<?php print base_path() . path_to_theme() ?>/images/button_next.gif" name="op" id="edit-submit" value="Log in" class="form-submit" />
 					<p class="forgot"><a href="<?php print base_path() ?>user/password" title="Forgot password?">Forgot password?</a></p>
-					<!--<input type="hidden" name="edit[form_id]" id="edit-form_id" value="user_login"  />-->
 					<input type="hidden" name="form_id" id="edit-user-login" value="user_login"  />
 				</div>
 			</form>
